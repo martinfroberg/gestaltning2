a2f76e601b075539e73af152ecf65c610313ec25
martinfroberg <martin.froberg@gmx.com>
2017-02-27 21:35:12 +0100

    Latest news marquee

---
+++ js/latestnewsscroll.js
@@ -0,0 +1,9 @@
+$('.latest-news-marquee').marquee({
+  delayBeforeStart: -9500,
+  direction: 'up',
+  duplicated: true,
+  duration: 10000,
+  gap: 15,
+  pauseOnCycle: true,
+  pauseOnHover: true
+});
