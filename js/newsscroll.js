cb630d38255c7d412054d6cfc5bc8b6fde3a21f6
martinfroberg <martin.froberg@gmx.com>
2017-02-27 22:02:49 +0100

    Fixed conflicts - removed ape zoom

--- js/newsscroll.js
+++ js/newsscroll.js
@@ -1,12 +1,7 @@
-
-$('.marquee').marquee({
-  duration: 15000,
-
 $('.trending-news-marquee').marquee({
   delayBeforeStart: -8000,
   duplicated: true,
   duration: 15000,
   gap: 0,
-
   pauseOnHover: true,
 });
