13a9d8443654d24af937990b115a9366a54bda60
martinfroberg <martin.froberg@gmx.com>
2017-02-28 21:23:14 +0100

    Footer + småpill
    Text storlekar etc

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,12 @@
 <footer>
-  Footer
+  <ul>
+    <li><a href="#">Terms of Use</a></li>
+    <li><a href="#">About Imperium</a></li>
+    <li><a href="#">Privacy Policy</a></li>
+    <li><a href="#">Parental Guidance</a></li>
+    <li><a href="#">Contact Imperium</a></li>
+    <li><a href="#">Advertise with us</a></li>
+    <li><a href="#">Ad choices</a></li>
+  </ul>
+  <small>Copyright © 2017 Imperium. Imperium is not responsible for the content of external sites. Read about our approach to external linking.</small>
 </footer>
