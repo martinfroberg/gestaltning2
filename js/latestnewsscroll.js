d37ec9888a5bae81b5cbe6a5449da2ad52401e50
Adam Roth <adam.roth96@gmail.com>
2017-03-01 12:57:43 +0100

    småfix

--- js/latestnewsscroll.js
+++ js/latestnewsscroll.js
@@ -2,7 +2,7 @@ $('.latest-news-marquee').marquee({
   delayBeforeStart: -9500,
   direction: 'up',
   duplicated: true,
-  duration: 10000,
+  duration: 20000,
   gap: 15,
   pauseOnCycle: true,
   pauseOnHover: true
